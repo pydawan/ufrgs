180e462becda1fef3c124b7e08920e4a919c7fb1
Germano Andersson <germanoa@gmail.com>
2014-10-06 21:44:16 -0300

    aed

--- avaliacao_desempenho/p1-scripts/filas.py
+++ avaliacao_desempenho/p1-scripts/filas.py
@@ -4,6 +4,7 @@ import math
 
 #convert scientific to decimal - http://www.webmath.com/sn_convert.html
 
+k=1
 
 #ex1 - gateway
 #y=125.0
@@ -11,9 +12,23 @@ import math
 #k=15.0
 
 #ex2 - datacenter
-y=240.0/60.0
-u=800.0/176.0
-k=1.0
+#y=240.0/60.0
+#u=800.0/176.0
+#k=1.0
+
+#ex1 - redes filas abertas
+#y=4
+#u=4.54
+#k=1.0
+
+#ex 5 filas caixa MM1
+#y=10.0/5.0
+#u=4.0
+
+#ex 5 filas caixa MMm
+y=10.0
+u=4.0*5.0
+
 
 dict_filas = {
     "y": "taxa de chegada de tarefas",
@@ -29,12 +44,12 @@ dict_filas = {
     "Erq": "(Tq) tempo medio de permanencia das tarefas na fila",
     }
 
-Et = 1/y
-Ets = 1/u
+Et = 1.0/y
+Ets = 1.0/u
 p = y/u
 U = p
-En = p/(1-p)
-Enq = p**2/(1-p)
+En = p/(1.0-p)
+Enq = p**2.0/(1.0-p)
 Pn = p**k
 Er = En/y #Teorema de Little
 Er = 1/(u-y)
@@ -42,9 +57,9 @@ Erq = Enq/y
 
 def pe(n):
     if (n==0):
-        return 1-p
+        return 1.0-p
     else:
-        return p^n(1-p)
+        return p^n(1.0-p)
 
 #determinar "k" onde P(n>=k)=x
 def ka(x):
